07a57c821333e721db8e3200d6e6c90e0053b284
Tibo <tibo.verreycken@telenet.be>
2024-11-01 23:02:01 +0100

    other gradle version maybe ?

--- src/test/java/com/commands/CompanyCTests.java
+++ src/test/java/com/commands/CompanyCTests.java
@@ -188,7 +188,6 @@ class CompanyCTests {
         boolean suc6 = server.execute("company", testPlayer, "give", "TCI", "1000").hasSucceeded();
         assertTrue(suc6);
 
-        System.out.print(testPlayer.nextMessage());
         testPlayer.assertSaid("§bplease re-enter your command to confirm\n" +
                 "that you want to give §f1.000§b to §fTCI\n" +
                 "§c/company give TCI 1000.0");
