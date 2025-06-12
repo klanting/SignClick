f9a0fad9788429842f5cd4ad2db1f3ed877b1c88
Tibo <tibo.verreycken@telenet.be>
2025-06-12 15:21:24 +0200

    Feat: Add system to check and ensure migrations between versions

---
+++ src/test/java/TestDataGenerator.java
@@ -0,0 +1,83 @@
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+import be.seeseemelk.mockbukkit.entity.PlayerMock;
+import com.klanting.signclick.SignClick;
+import com.klanting.signclick.economy.Company;
+import com.klanting.signclick.economy.Country;
+import com.klanting.signclick.economy.CountryManager;
+import com.klanting.signclick.economy.Market;
+import tools.ExpandedServerMock;
+import tools.TestTools;
+import java.io.File;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestDataGenerator {
+    public static void main(String[] args) throws IOException {
+        /*
+        * Script to generate config files for the current version
+        * */
+        ServerMock server = MockBukkit.mock(new ExpandedServerMock());;
+        SignClick plugin = TestTools.setupPlugin(server);
+
+        /*
+        * Create a company
+        * */
+        PlayerMock testPlayer = TestTools.addPermsPlayer(server, plugin);
+
+        SignClick.getEconomy().depositPlayer(testPlayer, 40000000);
+        Market.addCompany("TestCaseInc", "TCI", Market.getAccount(testPlayer));
+        SignClick.getEconomy().withdrawPlayer(testPlayer, 40000000);
+
+        /*
+         * Create a country
+         * */
+        CountryManager.create("empire1", testPlayer);
+
+        /*
+        * Server to Company Contract
+        * */
+
+        Company comp = Market.getCompany("TCI");
+        comp.addBal(1000000000.0);
+        comp.doUpgrade(0);
+
+        /*
+        * Reboot server
+        * */
+        TestTools.reboot(server);
+
+        /*
+        * Save configuration
+        * */
+        File virtualDataFolder = plugin.getDataFolder();
+
+        File realOutputFolder = new File("src/test/resources", "v101");
+
+        Files.walk(virtualDataFolder.toPath())
+                .forEach(source -> {
+
+                    Path relativePath = virtualDataFolder.toPath().relativize(source);
+                    Path destination = realOutputFolder.toPath().resolve(relativePath);
+
+                    System.out.println("Child: " + source + " | Is Directory? " + destination);
+
+                    try {
+                        Files.copy(source, destination);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+
+        /*
+        * Unmock bukkit
+        * */
+        MockBukkit.unmock();
+        Market.clear();
+    }
+}
